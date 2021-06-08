0437acdd1a77f2314883a9934bd31724b8202465
Yoedusvany <yoedusvany@gmail.com>
2021-06-08 17:59:40 -0400

    Auth added

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -20,3 +21,7 @@ Route::get('/', function () {
 Route::get('/test', function () {
     return view('test');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
